0cc006db682770fa25f6ff276c9593f87130cbcb
Aidan Grabe <aidsgrabe@gmail.com>
2014-04-26 13:50:33 +0100

    Gamescreen now loads as default screen

--- core/src/org/intothedeep/ld48/IntoTheDeep.java
+++ core/src/org/intothedeep/ld48/IntoTheDeep.java
@@ -2,26 +2,47 @@ package org.intothedeep.ld48;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import org.intothedeep.ld48.screens.GameScreen;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class IntoTheDeep extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+//    public static final int HEIGHT = 480;
+//    public static final int WIDTH = 320;
+
+    private static Map<ScreenName, Screen> screens;
+    private static Screen currentScreen;
+
+    public enum ScreenName {
+        GAME
+    }
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+        screens = new HashMap<ScreenName, Screen>();
+
+        screens.put(ScreenName.GAME, new GameScreen());
+
+        setScreen(ScreenName.GAME);
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+
+        currentScreen.render(Gdx.graphics.getDeltaTime());
 	}
+
+    public static void setScreen(ScreenName screenName) {
+        if (currentScreen != null) {
+            currentScreen.hide();
+        }
+        currentScreen = screens.get(screenName);
+        currentScreen.show();
+    }
 }

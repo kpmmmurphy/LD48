e149e6d3b554fe4b4113f98b541904110bd802b4
kpmmmurphy <kpmmmurphy@gmail.com>
2014-04-26 22:14:17 +0100

    Movement mofo

--- core/src/org/intothedeep/ld48/screens/GameScreen.java
+++ core/src/org/intothedeep/ld48/screens/GameScreen.java
@@ -1,15 +1,20 @@
 package org.intothedeep.ld48.screens;
 
-import org.intothedeep.ld48.entities.Background;
+import com.badlogic.gdx.Input;
+
+import org.intothedeep.ld48.entities.Bubble;
 import org.intothedeep.ld48.entities.Diver;
 import org.intothedeep.ld48.framework.Assets;
 import org.intothedeep.ld48.framework.BaseScreen;
+
 
 /**
  * Created by aidan on 26/04/14.
  */
 public class GameScreen extends BaseScreen {
     private State currentState;
+
+    private Diver diver;
 
     public enum State {
         READY, PAUSED, RUNNING, OVER
@@ -33,7 +38,7 @@ public class GameScreen extends BaseScreen {
                 presentPaused();
                 break;
             case RUNNING:
-                updateRunnng();
+                updateRunning();
                 presentRunning();
                 break;
             case OVER:
@@ -51,7 +56,7 @@ public class GameScreen extends BaseScreen {
 
     }
 
-    public void updateRunnng() {
+    public void updateRunning() {
 
     }
 
@@ -77,14 +82,37 @@ public class GameScreen extends BaseScreen {
 
     @Override
     public void show() {
-        stage.clear();
-        stage.addActor(new Background(this));
-        stage.addActor(new Diver(this, 60));
-        System.out.println("SHOWING++++++++++");
+
+        super.show();
+        diver = new Diver(this, 60);
+        stage.addActor(diver);
+
+        Bubble bubble = new Bubble(this, 60);
+        stage.addActor(bubble);
     }
 
     @Override
     public void hide() {
 
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        if(keycode == Input.Keys.DPAD_RIGHT){
+            diver.moveRight();
+        }
+
+        if(keycode == Input.Keys.DPAD_LEFT){
+            diver.moveLeft();
+        }
+
+        if(keycode == Input.Keys.DPAD_UP){
+            diver.moveUp();
+        }
+
+        if(keycode == Input.Keys.DPAD_DOWN){
+            diver.moveDown();
+        }
+        return false;
     }
 }

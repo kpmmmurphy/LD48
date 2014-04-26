0b1aefe0531325f5f570e0b811d2a472df5072c1
kpmmmurphy <kpmmmurphy@gmail.com>
2014-04-26 12:16:39 +0100

    First commit, bitch.

---
+++ desktop/src/org/intothedeep/ld48/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.intothedeep.ld48.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.intothedeep.ld48.IntoTheDeep;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new IntoTheDeep(), config);
+	}
+}
---
+++ html/src/org/intothedeep/ld48/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package org.intothedeep.ld48.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import org.intothedeep.ld48.IntoTheDeep;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new IntoTheDeep();
+        }
+}
